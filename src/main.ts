635ca3547666ddc32c43d69dc5e86b0c59d63bb0
John Emil <62772294+MawuliKing@users.noreply.github.com>
2025-09-05 01:04:33 +0000

    Enhance Swagger setup to persist authorization
    - Updated SwaggerModule setup in main.ts to include options for persisting authorization, improving API documentation usability.

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,11 @@ async function bootstrap() {
 
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   await app.listen(port, () => {
     console.log(`Server is running on port ${port}: http://localhost:${port}/api Access the API docs online at https://sikatrims.duckdns.org/api`);
